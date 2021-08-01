61bfe392a26ecd6776ac6b2f12739639a65ba9a7
+ <+>
2021-08-01 15:06:54 -0700

    my first attempt at a webpage

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 const h2 = document.createElement("h2");
-h2.textContent="This content added by JS";
+h2.textContent="";
 
 document.querySelector("body").appendChild(h2);
+
+
+let first=document.getElementById("firsttext");
+
+first.addEventListener("mouseover", function(e) {
+    e.target.style.color="yellow";
+
+    setTimeout(function() {
+        e.target.style.color="";
+    }, 500)
+}, false);
+
+
+
+
+/*firsttext.addEventListener('mouseover', function(){
+    header.style.width = '200px';
+    header.style.height = '300px';
+})
+*/
+
+
